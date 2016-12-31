fc94001697263834448dd74b7e6bb2070a719060
Eric Babin <ericbabin8@gmail.com>
2016-12-31 15:25:40 -0600

    NYE

---
+++ js/animation.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+$('#sec_1 img').mouseenter(function(){
+    $(this).addClass('screen');
+        });
+    $('#sec_1 img').mouseleave(function(){
+        $(this).removeClass('screen');
+        });
+
+        $('#sec_2 img').mouseenter(function(){
+            $(this).addClass('screen');
+                });
+            $('#sec_2 img').mouseleave(function(){
+                $(this).removeClass('screen');
+                });
+
+});
